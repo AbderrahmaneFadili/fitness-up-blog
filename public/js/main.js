6f745c7bcd7eceb7acf45a5d23db9d8a058a293b
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-11-09 17:30:48 +0100

    Create navbar

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
+/* Get Elements from the pages */
+const dropdownBlog = document.querySelector(".blog");
+const dropdownBlogMenu = document.querySelector(".blog + ul");
 
+const user = document.querySelector(".user");
+const dropdownUser = document.querySelector(".user + ul");
+
+dropdownBlog.addEventListener("click", (event) => {
+    dropdownBlogMenu.classList.toggle("hidden");
+});
+
+user.addEventListener("click", function () {
+    dropdownUser.classList.toggle("hidden");
+});
